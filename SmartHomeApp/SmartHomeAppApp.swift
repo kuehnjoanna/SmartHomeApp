31b276cd944fa7e64cd5e21eccd57093e4d3c875
Joanna Kuehn <kuehnjoanna@gmail.com>
2024-10-01 11:10:25 +0200

    Initial Commit

---
+++ SmartHomeApp/SmartHomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeAppApp.swift
+//  SmartHomeApp
+//
+//  Created by Joanna Kühn on 01.10.24.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
